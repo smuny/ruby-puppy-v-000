5138f823ff1f191d44d40a662fc17d38582a8cb4
Sharon Mun <sharonymun@gmail.com>
2018-07-31 04:44:47 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Dog 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def name
+    @name
+  end
+  def self.clear_all
+    @@all.clear
+  end
+  def self.all
+    @@all.each do |puppy|
+      puts puppy.name
+   end
+ end
+end
+
